e8f893e3eabf8af3897b525b3f640e94e39e95b1
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-11-29 21:02:16 +0300

    FindAlcaSubset: prints decls and assignment statements in main.

--- alcohol.sh
+++ alcohol.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 if [ "$1" = "clang" ]
 then
-    clang -cc1 -load build/libalcohol.so -plugin hello-world "$2"
+    clang -cc1 -load build/libalcohol.so -plugin alca-subset "$2"
 elif [ "$1" = "clang++" ]
 then
-    clang++ -c -Xclang -load -Xclang build/libalcohol.so -Xclang -plugin -Xclang hello-world "$2"
+    clang++ -c -Xclang -load -Xclang build/libalcohol.so -Xclang -plugin -Xclang alca-subset "$2"
 else
     echo "Usage: alcohol.sh [clang | clang++] file"
 fi
