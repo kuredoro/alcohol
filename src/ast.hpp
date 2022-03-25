2a6823f86883d21546a7b83564311ca3b6679f33
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-03-25 14:29:42 +0300

    ast: block and if_else nodes take arguments and print themselves.

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,11 +1,20 @@
 #pragma once
+
+#include <boost/mp11/algorithm.hpp>
+#include <boost/mp11/integral.hpp>
+#include <memory>
 #include <type_traits>
 #include <vector>
 #include <utility>
 #include <gsl/gsl-lite.hpp>
+#include <boost/mp11.hpp>
+
+#include <util.hpp>
 
 namespace ast
 {
+
+using namespace boost::mp11;
 
 struct statement
 {
@@ -30,6 +39,7 @@ struct nop final : public statement
     }
 };
 
+struct block final : public statement
 {
     template <class... Statements,
              bool ArgumentsAreStatements = (std::is_base_of<statement, std::remove_pointer_t<Statements>>::value && ...)
@@ -37,33 +47,72 @@ struct nop final : public statement
     explicit block(Statements&&... statements)
     {
         static_assert(ArgumentsAreStatements, "The constructor arguments should be derived from ast::statement");
+
+        statements_.resize(sizeof...(statements));
+
+        auto&& insert = [this](size_t i, auto&& stmt)
+        {
+            statements_[i] = util::make_unique_from_ref<decltype(stmt)>(std::forward<decltype(stmt)>(stmt));
+        };
+
+        util::for_each_argument(insert, std::forward<Statements>(statements)...);
+    }
+
+    std::string to_string() const override
+    {
+        std::string result = "begin\n";
+        for (auto& stmt : statements_)
+        {
+            result += stmt->to_string();
+        }
+        
+        return result + "end\n";
     }
 
 private:
-    std::vector<statement> statements_;
+    std::vector<std::unique_ptr<statement>> statements_;
 };
 
 struct if_else final : public statement
 {
     template <class TrueBranch,
-             bool = std::is_base_of<statement, std::remove_pointer_t<TrueBranch>>::value
+             bool TrueBranchIsStatement = std::is_base_of<statement, std::remove_pointer_t<TrueBranch>>::value
     >
     explicit if_else(TrueBranch&& trueBranch) :
-        trueBranch_(std::forward<TrueBranch>(trueBranch))
-    {}
+        trueBranch_(std::make_unique<TrueBranch>(std::forward<TrueBranch>(trueBranch)))
+    {
+        static_assert(TrueBranchIsStatement, "the true branch must be a statement");
+    }
 
     template <class TrueBranch,
              class FalseBranch,
-             bool = std::is_base_of<statement, std::remove_pointer_t<TrueBranch>>::value,
-             bool = std::is_base_of<statement, std::remove_pointer_t<FalseBranch>>::value
+             bool TrueBranchIsStatement = std::is_base_of<statement, std::remove_pointer_t<TrueBranch>>::value,
+             bool FalseBranchIsStatement = std::is_base_of<statement, std::remove_pointer_t<FalseBranch>>::value
     >
     if_else(TrueBranch&& trueBranch, FalseBranch&& falseBranch) :
-        trueBranch_(std::forward<TrueBranch>(trueBranch)),
-        falseBranch_(std::forward<FalseBranch>(falseBranch))
-    {}
+        trueBranch_(std::make_unique<TrueBranch>(std::forward<TrueBranch>(trueBranch))),
+        falseBranch_(std::make_unique<FalseBranch>(std::forward<FalseBranch>(falseBranch)))
+    {
+        static_assert(TrueBranchIsStatement, "the true branch must be a statement");
+        static_assert(FalseBranchIsStatement, "the false branch must be a statement");
+    }
+
+    std::string to_string() const
+    {
+        std::string result = "if ... begin\n";
+        if (trueBranch_)
+            result += trueBranch_->to_string();
+
+        if (falseBranch_)
+            result += "else\n" + falseBranch_->to_string();
+
+        result += "end\n";
+
+        return result;
+    }
 
 private:
-    statement trueBranch_{}, falseBranch_{};
+    std::unique_ptr<statement> trueBranch_{}, falseBranch_{};
 };
 
 struct while_loop final : public statement
