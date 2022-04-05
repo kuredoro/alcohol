7e44e150b99b9ba3ca12856d3bce0824ee70d5d4
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-04-05 13:42:18 +0300

    ast: made expressions visitable.

--- src/ast/expressions.cpp
+++ src/ast/expressions.cpp
@@ -1,7 +1,13 @@
+#include "ast/manager.hpp"
 #include <ast/expressions.hpp>
 
 namespace ast
 {
+
+void expression::accept(expression_visitor& visitor)
+{
+    visitor.process(*this);
+}
 
 bool operator==(const expression& left, const expression& right)
 {
