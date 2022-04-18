040cfc8d6dc1f86092104677f2af4230da1e488b
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-04-18 15:06:35 +0300

    constraint_set: can parse int == int constraints and check satisfiability of a single one.

--- src/constraint/set.cpp
+++ src/constraint/set.cpp
@@ -1,11 +1,99 @@
 #include <constraint/set.hpp>
 
+#include <ast/expressions.hpp>
+
+#include <z3++.h>
+
+#include <stdexcept>
+
 namespace constraint
 {
 
+void set::add(ast::constraint* expr)
+{
+    constraints_.push_back(expr);
+}
+
 bool set::check_consistency() const
 {
-    return true;
+    z3::context ctx;
+    z3::solver solver(ctx);
+
+    for (auto& c : constraints_)
+    {
+        solver.add(to_z3_form(ctx, c));
+    }
+
+    switch (solver.check())
+    {
+    case z3::sat:
+        return true;
+    case z3::unsat:
+        return false;
+    default:
+        throw std::runtime_error("constraint_set: check_consistency: z3 failed");
+        return false;
+    }
+}
+
+z3::expr to_z3_form(z3::context& c, ast::expression* expr)
+{
+    detail::to_z3_form_visitor visitor(c);
+    expr->accept(visitor);
+
+    return visitor.result();
+}
+
+namespace detail
+{
+    void to_z3_form_visitor::process(ast::expression&)
+    {
+        stack_.push_back(ctx_.int_const("foo") == ctx_.int_const("foo"));
+    }
+
+    void to_z3_form_visitor::process(ast::var& var)
+    {
+        process(static_cast<ast::expression&>(var));
+    }
+
+    void to_z3_form_visitor::process(ast::integer& integer)
+    {
+        stack_.push_back(ctx_.int_val(integer.value()));
+    }
+
+    void to_z3_form_visitor::process(ast::add& sum)
+    {
+        process(static_cast<ast::expression&>(sum));
+    }
+
+    void to_z3_form_visitor::process(ast::multiply& product)
+    {
+        process(static_cast<ast::expression&>(product));
+    }
+
+    void to_z3_form_visitor::process(ast::constraint& constraint)
+    {
+        constraint.left()->accept(*this);
+        z3::expr left = stack_.back();
+        stack_.pop_back();
+
+        constraint.right()->accept(*this);
+        z3::expr right = stack_.back();
+        stack_.pop_back();
+
+        switch (constraint.kind())
+        {
+        case ast::constraint::relation::eq:
+            stack_.push_back(left == right);
+            break;
+        case ast::constraint::relation::neq:
+            stack_.push_back(left != right);
+            break;
+        default:
+            break;
+        }
+    }
+
 }
 
 }
--- src/constraint/set.hpp
+++ src/constraint/set.hpp
@@ -2,14 +2,45 @@
 
 #include <ast/expressions.hpp>
 
+#include <z3++.h>
+
 namespace constraint
 {
 
 struct set
 {
-    void add(ast::constraint*) {}
+    void add(ast::constraint*);
     bool check_satisfiability_with(ast::constraint*) const { return true; }
     bool check_consistency() const;
+
+private:
+    std::vector<ast::constraint*> constraints_;
 };
+
+z3::expr to_z3_form(z3::context&, ast::expression*);
+
+namespace detail
+{
+    struct to_z3_form_visitor : public ast::expression_visitor
+    {
+        explicit to_z3_form_visitor(z3::context& ctx) : ctx_(ctx) {}
+
+        void process(ast::expression&) override;
+        void process(ast::var&) override;
+        void process(ast::integer&) override;
+        void process(ast::add&) override;
+        void process(ast::multiply&) override;
+        void process(ast::constraint&) override;
+
+        z3::expr result() const
+        {
+            return stack_.back();
+        }
+
+    private:
+        z3::context& ctx_;
+        std::vector<z3::expr> stack_;
+    };
+}
 
 }
