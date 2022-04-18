f031dc492a35a6ac89284bcaa2f7f8bea981b351
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-04-18 16:05:49 +0300

    constraint_set: refactored check_consistency tests to be in a table.

--- src/constraint/set.test.cpp
+++ src/constraint/set.test.cpp
@@ -6,167 +6,173 @@
 
 using namespace boost::ut;
 
+struct consistency_test_case
+{
+    bool wantConsistent;
+    std::vector<ast::constraint*> constraints;
+};
+
 int main()
 {
-    "check_consistency"_test = [] () {
-        ast::manager store;
+    ast::manager store;
 
-        should("empty set is consistent") = [&] () {
+    std::vector<consistency_test_case> checkConsistencyCases{
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::integer(store, 42),
+                    ast::integer(store, 42)
+                ),
+            },
+        },
+        {
+            false,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::neq,
+                    ast::integer(store, 0),
+                    ast::integer(store, 0)
+                ),
+            },
+        },
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::var(store, "foo"),
+                    ast::var(store, "foo")
+                ),
+            },
+        },
+        {
+            false,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::neq,
+                    ast::var(store, "foo"),
+                    ast::var(store, "foo")
+                ),
+            },
+        },
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::add(store,
+                        ast::var(store, "foo"), ast::var(store, "bar")
+                    ),
+                    ast::var(store, "foo")
+                ),
+            },
+        },
+        {
+            false,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::add(store,
+                        ast::var(store, "foo"), ast::integer(store, 0)
+                    ),
+                    ast::add(store,
+                        ast::var(store, "foo"), ast::integer(store, 1)
+                    )
+                ),
+            },
+        },
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::neq,
+                    ast::multiply(store,
+                        ast::integer(store, 2), ast::var(store, "foo")
+                    ),
+                    ast::multiply(store,
+                        ast::integer(store, 3), ast::var(store, "foo")
+                    )
+                ),
+            },
+        },
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::multiply(store,
+                        ast::add(store,
+                            ast::var(store, "foo"), ast::integer(store, 1)
+                        ),
+                        ast::add(store,
+                            ast::var(store, "bar"), ast::integer(store, 1)
+                        )
+                    ),
+                    ast::multiply(store,
+                        ast::var(store, "foo"), ast::var(store, "bar")
+                    )
+                ),
+            },
+        },
+        {
+            true,
+            {
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::var(store, "foo"),
+                    ast::var(store, "bar")
+                ),
+                store.make_expression<ast::constraint>(
+                    ast::constraint::relation::eq,
+                    ast::integer(store, 42),
+                    ast::integer(store, 42)
+                ),
+            },
+        },
+    };
+
+    "default constructed"_test = [] () {
+        should("is consistent") = [&] () {
             constraint::set constraints;
             expect(constraints.check_consistency());
         };
+    };
 
-        should("int1 == int1 is consistent") = [&] () {
-            constraint::set constraints;
+    "check_consistency"_test = [&] () {
+        // TODO: range-v3
+        for (auto& testCase : checkConsistencyCases)
+        {
+            std::string testName;
+            for (size_t i = 0; i < testCase.constraints.size(); i++)
+            {
+                if (i != 0)
+                    testName += " && ";
 
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::integer(store, 42),
-                ast::integer(store, 42)
-            );
+                testName += testCase.constraints[i]->to_string();
+            }
 
-            constraints.add(expr);
+            if (testCase.wantConsistent)
+            {
+                testName += " is consistent";
+            }
+            else
+            {
+                testName += " is inconsistent";
+            }
 
-            expect(constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
+            should(testName) = [&] () {
+                constraint::set constraints;
 
-        should("int1 != int1 is inconsistent") = [&] () {
-            constraint::set constraints;
+                for (auto& constraint : testCase.constraints)
+                {
+                    constraints.add(constraint);
+                }
 
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::neq,
-                ast::integer(store, 0),
-                ast::integer(store, 0)
-            );
-
-            constraints.add(expr);
-
-            expect(!constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("var == var is consistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::var(store, "foo"),
-                ast::var(store, "foo")
-            );
-
-            constraints.add(expr);
-
-            expect(constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("var != var is inconsistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::neq,
-                ast::var(store, "bar"),
-                ast::var(store, "bar")
-            );
-
-            constraints.add(expr);
-
-            expect(!constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("foo + bar == foo is consistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::add(store,
-                    ast::var(store, "foo"), ast::var(store, "bar")
-                ),
-                ast::var(store, "foo")
-            );
-
-            constraints.add(expr);
-
-            expect(constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("foo + 0 == foo + 1 is inconsistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::add(store,
-                    ast::var(store, "foo"), ast::integer(store, 0)
-                ),
-                ast::add(store,
-                    ast::var(store, "foo"), ast::integer(store, 1)
-                )
-            );
-
-            constraints.add(expr);
-
-            expect(!constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("2 * foo != 3 * foo is consistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::neq,
-                ast::multiply(store,
-                    ast::integer(store, 2), ast::var(store, "foo")
-                ),
-                ast::multiply(store,
-                    ast::integer(store, 3), ast::var(store, "foo")
-                )
-            );
-
-            constraints.add(expr);
-
-            expect(constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("(foo + 1) * (bar + 1) == foo * bar is consistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::multiply(store,
-                    ast::add(store,
-                        ast::var(store, "foo"), ast::integer(store, 1)
-                    ),
-                    ast::add(store,
-                        ast::var(store, "bar"), ast::integer(store, 1)
-                    )
-                ),
-                ast::multiply(store,
-                    ast::var(store, "foo"), ast::var(store, "bar")
-                )
-            );
-
-            constraints.add(expr);
-
-            expect(constraints.check_consistency()) << "for a single constraint:" << expr->to_string();
-        };
-
-        should("foo == foo && 42 == 42 is consistent") = [&] () {
-            constraint::set constraints;
-
-            auto expr1 = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::var(store, "foo"),
-                ast::var(store, "bar")
-            );
-
-            auto expr2 = store.make_expression<ast::constraint>(
-                ast::constraint::relation::eq,
-                ast::integer(store, 42),
-                ast::integer(store, 42)
-            );
-
-            constraints.add(expr1);
-            constraints.add(expr2);
-
-            expect(constraints.check_consistency()) << "for 2 constraints:" << expr1->to_string() << " && " << expr2->to_string() << '\n';
-        };
+                expect(constraints.check_consistency() == testCase.wantConsistent);
+            };
+        }
     };
 
     return 0;
