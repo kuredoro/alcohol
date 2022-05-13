515783b876d7405c9e88075bea38ae484c999c74
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-05-14 01:54:42 +0300

    plugin: correct path to the plugin in the run script + changed from old naming convention.

--- alcohol.sh
+++ alcohol.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 if [ "$1" = "clang" ]
 then
-    clang -cc1 -load build/libalcohol.so -plugin alcohol "$2"
+    clang -cc1 -load build/lib/libalcohol.so -plugin alcohol "$2"
 elif [ "$1" = "clang++" ]
 then
     clang++ -c -Xclang -load -Xclang build/libalcohol.so -Xclang -plugin -Xclang alcohol "$2"
