746b15198abdc30d9366bf669c850ab4d50d4ecd
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-05-14 04:07:58 +0300

    plugin: parses decl statements with arbitrary expressions.

--- src/alc/plugin/AftFuncDeclConsumer.cpp
+++ src/alc/plugin/AftFuncDeclConsumer.cpp
@@ -1,4 +1,52 @@
+#include "alc/ast/expressions.hpp"
+#include "alc/ast/manager.hpp"
 #include <alc/plugin/AftFuncDeclConsumer.hpp>
+#include <alc/ast/statements.hpp>
+#include <clang/AST/Expr.h>
+
+ast::expression* to_alc_expression(ast::manager& store, const Expr* expr)
+{
+    auto implicitCast = dyn_cast<ImplicitCastExpr>(expr);
+    if (implicitCast)
+    {
+        return to_alc_expression(store, implicitCast->getSubExpr());
+    }
+
+    auto intLit = dyn_cast<const IntegerLiteral>(expr);
+    if (intLit)
+    {
+        return store.make_expression<ast::integer>(intLit->getValue().getZExtValue());
+    }
+
+    auto declRef = dyn_cast<DeclRefExpr>(expr);
+    if (declRef)
+    {
+        auto varName = declRef->getDecl()->getNameAsString();
+        return store.make_expression<ast::var>(varName);
+    }
+
+    auto binExpr = dyn_cast<BinaryOperator>(expr);
+    if (binExpr)
+    {
+        auto lhs = to_alc_expression(store, binExpr->getLHS());
+        if (!lhs)
+            return nullptr;
+
+        auto rhs = to_alc_expression(store, binExpr->getRHS());
+        if (!rhs)
+            return nullptr;
+
+        if (binExpr->getOpcode() == BinaryOperator::Opcode::BO_Add)
+            return store.make_expression<ast::add>(lhs, rhs);
+
+        if (binExpr->getOpcode() == BinaryOperator::Opcode::BO_Mul)
+            return store.make_expression<ast::multiply>(lhs, rhs);
+
+        return nullptr;
+    }
+
+    return nullptr;
+}
 
 void MatchMemoryAllocationCallback::run(const MatchFinder::MatchResult& result)
 {
@@ -27,4 +75,28 @@ void MatchMemoryDeallocationCallback::run(const MatchFinder::MatchResult& result
     auto dispose = store.make_statement<ast::dispose>(decl->getNameAsString());
 
     visitor.push_statement(dispose, {});
+}
+
+void MatchVariableDeclarationCallback::run(const MatchFinder::MatchResult& result)
+{
+    llvm::outs() << "Var decl!\n";
+
+    auto& nodes = result.Nodes;
+
+    auto varDecl = nodes.getNodeAs<VarDecl>("varDecl");
+    auto clangExpr = nodes.getNodeAs<Expr>("expr");
+
+    auto& store = visitor.ast_store();
+    auto alcExpr = to_alc_expression(store, clangExpr);
+
+    if (!alcExpr)
+    {
+        llvm::outs() << "Unsupported expression:\n";
+        varDecl->dump();
+        return;
+    }
+
+    auto decl = store.make_statement<ast::decl>(varDecl->getNameAsString(), alcExpr);
+
+    visitor.push_statement(decl, {});
 }
