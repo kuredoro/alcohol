ce14001008fd61d157905f3480ecb02d86fc85c1
siz6 <66309775+kuredoro@users.noreply.github.com>
2022-05-14 05:29:29 +0300

    plugin: removed for loops from big_array example.

--- _examples/big_arrays.c
+++ _examples/big_arrays.c
@@ -3,14 +3,20 @@
 int main()
 {
     int* x = (int*)malloc(6 * sizeof(int));
-    for (int i = 0; i < 6; i++)
-        // Potential nullptr
-        x[i] = i + 1;
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+    x[3] = 4;
+    x[4] = 5;
+    x[5] = 6;
 
     int* y = (int*)malloc(6 * sizeof(int));
-    for (int i = 0; i < 6; i++)
-        // Potential nullptr
-        y[i] = i + 1;
+    y[0] = 1;
+    y[1] = 2;
+    y[2] = 3;
+    y[3] = 4;
+    y[4] = 5;
+    y[5] = 6;
 
     free(x);
     free(y);
